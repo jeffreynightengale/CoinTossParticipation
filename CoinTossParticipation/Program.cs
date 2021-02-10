f21f834aafb90c668047d6d03080eda80c1538a0
Jeff <Jeff@DESKTOP-DFBAMBV>
2021-02-09 18:37:32 -0600

    I updated my participation so that it reads if user wants to pick heads or tails

--- CoinTossParticipation/Program.cs
+++ CoinTossParticipation/Program.cs
@@ -6,7 +6,25 @@ namespace CoinTossParticipation
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine($"Hey user, please pick heads or tails. >>");
+            string answer = Console.ReadLine();
+
+            Random rand = new Random();
+            int randomNum = rand.Next(1, 3); //generate a random # between 1 and 2
+
+            Console.WriteLine(randomNum);
+
+            if (randomNum == 1)
+            {
+                Console.WriteLine("Your coin landed on heads!");
+            }
+            else
+            {
+                Console.WriteLine("Your coin landed on tails!");
+            }
         }
+      
+            
+	}
     }
-}
+
